f14bdd8c0530b45bc534b27d17056e48027afe74
Cameron Wiley <cswiley1@gmail.com>
2023-05-03 20:49:53 -0400

    load balancer

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a04bd07a0fa7844f88220f3eb670bda3-1917796208.us-east-2.elb.amazonaws.com",
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a04bd07a0fa7844f88220f3eb670bda3-1917796208.us-east-2.elb.amazonaws.com'
 };
 
 /*
